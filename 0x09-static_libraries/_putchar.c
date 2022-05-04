21390788aae19b895abc7348b3797ee51eefe19d
clementus360 <clementus360@gmail.com>
2022-05-04 22:57:49 +0000

    static libraries tasks

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,4 +1,3 @@
-#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,5 +7,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+
 }
